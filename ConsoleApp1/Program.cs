b2052cd7859e42f3d7a5823b0f8d36b5bdcf00d0
Bilal <ozbil3434@gmail.com>
2024-03-05 10:27:13 +0300

    n

--- ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Application.Services.Repositories;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Persistence.Contexts;
-using Persistence.Repositories;
-
